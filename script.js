b52eef94483a74f8b2cc4b5d170ae96d5c735e96
d1ss <hfyd4006@gmail.com>
2026-04-14 18:22:51 +0300

    script.js

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ if (text === "C") {
       document.getElementById("taskInput").value = "";
     } 
     else if (text === "=") {
-      // Вычисляем и сразу превращаем в строку для стабильности
       display = eval(display).toString();
       document.getElementById("taskInput").value = display;
     } 
     else {
-      // Просто добавляем нажатый символ
       display += text;
       document.getElementById("taskInput").value = display;
     }

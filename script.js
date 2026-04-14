e79167abe9177a46dbb3b7a54e1168872bc24df0
d1ss <hfyd4006@gmail.com>
2026-04-14 18:08:28 +0300

    script.js 3ver

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+if (text === "C") {
+      display = "";
+      document.getElementById("taskInput").value = "";
+    } 
+    else if (text === "=") {
+      // Вычисляем и сразу превращаем в строку для стабильности
+      display = eval(display).toString();
+      document.getElementById("taskInput").value = display;
+    } 
+    else {
+      // Просто добавляем нажатый символ
+      display += text;
+      document.getElementById("taskInput").value = display;
+    }
+  });
+});
